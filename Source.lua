fa94c7f905f44d33bab8d2c08a0128a96fb9a409
xshad0xx <114771213+xshad0xx@users.noreply.github.com>
2022-11-18 16:52:22 +0100

    Create Source.lua

---
+++ Source.lua
@@ -0,0 +1,189 @@
+--[[
+
+ /$$$$$$$  /$$      /$$ /$$      /$$                                                                                                                           
+| $$__  $$| $$  /$ | $$| $$$    /$$$                                                                                                                           
+| $$  \ $$| $$ /$$$| $$| $$$$  /$$$$                                                                                                                           
+| $$  | $$| $$/$$ $$ $$| $$ $$/$$ $$                                                                                                                           
+| $$  | $$| $$$$_  $$$$| $$  $$$| $$                                                                                                                           
+| $$  | $$| $$$/ \  $$$| $$\  $ | $$                                                                                                                           
+| $$$$$$$/| $$/   \  $$| $$ \/  | $$                                                                                                                           
+|_______/ |__/     \__/|__/     |__/                                                                                                                           
+                                                                                                                                                               
+                                                                                                                                                               
+                                                                                                                                                               
+ /$$      /$$                 /$$                 /$$$$$$$                                                                                                     
+| $$$    /$$$                | $$                | $$__  $$                                                                                                    
+| $$$$  /$$$$  /$$$$$$   /$$$$$$$  /$$$$$$       | $$  \ $$ /$$   /$$                                                                                          
+| $$ $$/$$ $$ |____  $$ /$$__  $$ /$$__  $$      | $$$$$$$ | $$  | $$                                                                                          
+| $$  $$$| $$  /$$$$$$$| $$  | $$| $$$$$$$$      | $$__  $$| $$  | $$                                                                                          
+| $$\  $ | $$ /$$__  $$| $$  | $$| $$_____/      | $$  \ $$| $$  | $$                                                                                          
+| $$ \/  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$      | $$$$$$$/|  $$$$$$$                                                                                          
+|__/     |__/ \_______/ \_______/ \_______/      |_______/  \____  $$                                                                                          
+                                                            /$$  | $$                                                                                          
+                                                           |  $$$$$$/                                                                                          
+                                                            \______/                                                                                           
+ /$$$$$$$$                                 /$$     /$$           /$$      /$$                                 /$$ /$$    /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$ 
+| $$_____/                                | $$    |__/          | $$$    /$$$                                / $$/ $$   /$$__  $$ /$$__  $$ /$$__  $$| $$____/ 
+| $$        /$$$$$$  /$$   /$$  /$$$$$$  /$$$$$$   /$$  /$$$$$$$| $$$$  /$$$$  /$$$$$$   /$$$$$$  /$$$$$$$  /$$$$$$$$$$| $$  \ $$|__/  \ $$|__/  \ $$| $$      
+| $$$$$    /$$__  $$| $$  | $$ /$$__  $$|_  $$_/  | $$ /$$_____/| $$ $$/$$ $$ /$$__  $$ /$$__  $$| $$__  $$|   $$  $$_/|  $$$$$$/  /$$$$$$/   /$$$$$/| $$$$$$$ 
+| $$__/   | $$  \ $$| $$  | $$| $$  \ $$  | $$    | $$| $$      | $$  $$$| $$| $$  \ $$| $$  \ $$| $$  \ $$ /$$$$$$$$$$ >$$__  $$ /$$____/   |___  $$|_____  $$
+| $$      | $$  | $$| $$  | $$| $$  | $$  | $$ /$$| $$| $$      | $$\  $ | $$| $$  | $$| $$  | $$| $$  | $$|_  $$  $$_/| $$  \ $$| $$       /$$  \ $$ /$$  \ $$
+| $$$$$$$$|  $$$$$$$|  $$$$$$$| $$$$$$$/  |  $$$$/| $$|  $$$$$$$| $$ \/  | $$|  $$$$$$/|  $$$$$$/| $$  | $$  | $$| $$  |  $$$$$$/| $$$$$$$$|  $$$$$$/|  $$$$$$/
+|________/ \____  $$ \____  $$| $$____/    \___/  |__/ \_______/|__/     |__/ \______/  \______/ |__/  |__/  |__/|__/   \______/ |________/ \______/  \______/ 
+           /$$  \ $$ /$$  | $$| $$                                                                                                                             
+          |  $$$$$$/|  $$$$$$/| $$                                                                                                                             
+           \______/  \______/ |__/                                                                                                                             
+
+]]
+
+local library = {}
+
+function library:Setup(HardwareID)
+    local dwm = {}
+
+    function dwm:Send(Title, Webhook, Color)
+        if Color ~= nil then
+            if HardwareID then
+                local responce = syn.request(
+                    {
+                        Url = Webhook,
+                        Method = 'POST',
+                        Headers = {
+                            ['Content-Type'] = 'application/json'
+                        },
+                        Body = game:GetService("HttpService"):JSONEncode({
+                            ["content"] = "",
+                            ["embeds"] = {{
+                                ["title"] = "**"..Title.." has been executed!**",
+                                ["description"] = "https://github.com",
+                                ["type"] = "rich",
+                                ["color"] = Color,
+                                ["fields"] = {
+                                    {
+                                        ["name"] = "Display Name:",
+                                        ["value"] = game.Players.LocalPlayer.DisplayName,
+                                        ["inline"] = true
+                                    },
+                                    {
+                                        ["name"] = "User Name:",
+                                        ["value"] = game.Players.LocalPlayer.Name,
+                                        ["inline"] = true
+                                    },
+                                    {
+                                        ["name"] = "Hardware ID:",
+                                        ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
+                                        ["inline"] = true
+                                    }
+                                }
+                            }}
+                        })
+                    }
+                )
+            else
+                local responce = syn.request(
+                    {
+                        Url = Webhook,
+                        Method = 'POST',
+                        Headers = {
+                            ['Content-Type'] = 'application/json'
+                        },
+                        Body = game:GetService("HttpService"):JSONEncode({
+                            ["content"] = "",
+                            ["embeds"] = {{
+                                ["title"] = "**"..Title.." has been executed!**",
+                                ["description"] = "https://github.com",
+                                ["type"] = "rich",
+                                ["color"] = Color,
+                                ["fields"] = {
+                                    {
+                                        ["name"] = "Display Name:",
+                                        ["value"] = game.Players.LocalPlayer.DisplayName,
+                                        ["inline"] = true
+                                    },
+                                    {
+                                        ["name"] = "User Name:",
+                                        ["value"] = game.Players.LocalPlayer.Name,
+                                        ["inline"] = true
+                                    }
+                                }
+                            }}
+                        })
+                    }
+                )
+            end
+        else
+            if HardwareID then
+                local responce = syn.request(
+                    {
+                        Url = Webhook,
+                        Method = 'POST',
+                        Headers = {
+                            ['Content-Type'] = 'application/json'
+                        },
+                        Body = game:GetService("HttpService"):JSONEncode({
+                            ["content"] = "",
+                            ["embeds"] = {{
+                                ["title"] = "**"..Title.." has been executed!**",
+                                ["description"] = "https://github.com",
+                                ["type"] = "rich",
+                                ["color"] = tonumber(0xffffff),
+                                ["fields"] = {
+                                    {
+                                        ["name"] = "Display Name:",
+                                        ["value"] = game.Players.LocalPlayer.DisplayName,
+                                        ["inline"] = true
+                                    },
+                                    {
+                                        ["name"] = "User Name:",
+                                        ["value"] = game.Players.LocalPlayer.Name,
+                                        ["inline"] = true
+                                    },
+                                    {
+                                        ["name"] = "Hardware ID:",
+                                        ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
+                                        ["inline"] = true
+                                    }
+                                }
+                            }}
+                        })
+                    }
+                )
+            else
+                local responce = syn.request(
+                    {
+                        Url = Webhook,
+                        Method = 'POST',
+                        Headers = {
+                            ['Content-Type'] = 'application/json'
+                        },
+                        Body = game:GetService("HttpService"):JSONEncode({
+                            ["content"] = "",
+                            ["embeds"] = {{
+                                ["title"] = "**"..Title.." has been executed!**",
+                                ["description"] = "https://github.com",
+                                ["type"] = "rich",
+                                ["color"] = tonumber(0xffffff),
+                                ["fields"] = {
+                                    {
+                                        ["name"] = "Display Name:",
+                                        ["value"] = game.Players.LocalPlayer.DisplayName,
+                                        ["inline"] = true
+                                    },
+                                    {
+                                        ["name"] = "User Name:",
+                                        ["value"] = game.Players.LocalPlayer.Name,
+                                        ["inline"] = true
+                                    }
+                                }
+                            }}
+                        })
+                    }
+                )
+            end
+        end
+    end
+
+    return dwm
+end
+
+return library
