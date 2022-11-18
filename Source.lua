204ddfa0c0cf1edb5412c1f20f116fcfa5a47736
xshad0xx <114771213+xshad0xx@users.noreply.github.com>
2022-11-18 17:08:55 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -36,154 +36,163 @@
 
 ]]
 
-local library = {}
+if syn.request then
+    local library = {}
 
-function library:Setup(HardwareID)
-    local dwm = {}
+    function library:Setup(HardwareID)
+        local dwm = {}
 
-    function dwm:Send(Title, Webhook, Color)
-        if Color ~= nil then
-            if HardwareID then
-                local responce = syn.request(
-                    {
-                        Url = Webhook,
-                        Method = 'POST',
-                        Headers = {
-                            ['Content-Type'] = 'application/json'
-                        },
-                        Body = game:GetService("HttpService"):JSONEncode({
-                            ["content"] = "",
-                            ["embeds"] = {{
-                                ["title"] = "**"..Title.." has been executed!**",
-                                ["description"] = "https://github.com",
-                                ["type"] = "rich",
-                                ["color"] = Color,
-                                ["fields"] = {
-                                    {
-                                        ["name"] = "Display Name:",
-                                        ["value"] = game.Players.LocalPlayer.DisplayName,
-                                        ["inline"] = true
-                                    },
-                                    {
-                                        ["name"] = "User Name:",
-                                        ["value"] = game.Players.LocalPlayer.Name,
-                                        ["inline"] = true
-                                    },
-                                    {
-                                        ["name"] = "Hardware ID:",
-                                        ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
-                                        ["inline"] = true
+        function dwm:Send(Title, Webhook, Color)
+            if Color ~= nil then
+                if HardwareID then
+                    local responce = syn.request(
+                        {
+                            Url = Webhook,
+                            Method = 'POST',
+                            Headers = {
+                                ['Content-Type'] = 'application/json'
+                            },
+                            Body = game:GetService("HttpService"):JSONEncode({
+                                ["content"] = "",
+                                ["embeds"] = {{
+                                    ["title"] = "**"..Title.." has been executed!**",
+                                    ["description"] = "https://github.com",
+                                    ["type"] = "rich",
+                                    ["color"] = Color,
+                                    ["fields"] = {
+                                        {
+                                            ["name"] = "Display Name:",
+                                            ["value"] = game.Players.LocalPlayer.DisplayName,
+                                            ["inline"] = true
+                                        },
+                                        {
+                                            ["name"] = "User Name:",
+                                            ["value"] = game.Players.LocalPlayer.Name,
+                                            ["inline"] = true
+                                        },
+                                        {
+                                            ["name"] = "Hardware ID:",
+                                            ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
+                                            ["inline"] = true
+                                        }
                                     }
-                                }
-                            }}
-                        })
-                    }
-                )
+                                }}
+                            })
+                        }
+                    )
+                else
+                    local responce = syn.request(
+                        {
+                            Url = Webhook,
+                            Method = 'POST',
+                            Headers = {
+                                ['Content-Type'] = 'application/json'
+                            },
+                            Body = game:GetService("HttpService"):JSONEncode({
+                                ["content"] = "",
+                                ["embeds"] = {{
+                                    ["title"] = "**"..Title.." has been executed!**",
+                                    ["description"] = "https://github.com",
+                                    ["type"] = "rich",
+                                    ["color"] = Color,
+                                    ["fields"] = {
+                                        {
+                                            ["name"] = "Display Name:",
+                                            ["value"] = game.Players.LocalPlayer.DisplayName,
+                                            ["inline"] = true
+                                        },
+                                        {
+                                            ["name"] = "User Name:",
+                                            ["value"] = game.Players.LocalPlayer.Name,
+                                            ["inline"] = true
+                                        }
+                                    }
+                                }}
+                            })
+                        }
+                    )
+                end
             else
-                local responce = syn.request(
-                    {
-                        Url = Webhook,
-                        Method = 'POST',
-                        Headers = {
-                            ['Content-Type'] = 'application/json'
-                        },
-                        Body = game:GetService("HttpService"):JSONEncode({
-                            ["content"] = "",
-                            ["embeds"] = {{
-                                ["title"] = "**"..Title.." has been executed!**",
-                                ["description"] = "https://github.com",
-                                ["type"] = "rich",
-                                ["color"] = Color,
-                                ["fields"] = {
-                                    {
-                                        ["name"] = "Display Name:",
-                                        ["value"] = game.Players.LocalPlayer.DisplayName,
-                                        ["inline"] = true
-                                    },
-                                    {
-                                        ["name"] = "User Name:",
-                                        ["value"] = game.Players.LocalPlayer.Name,
-                                        ["inline"] = true
+                if HardwareID then
+                    local responce = syn.request(
+                        {
+                            Url = Webhook,
+                            Method = 'POST',
+                            Headers = {
+                                ['Content-Type'] = 'application/json'
+                            },
+                            Body = game:GetService("HttpService"):JSONEncode({
+                                ["content"] = "",
+                                ["embeds"] = {{
+                                    ["title"] = "**"..Title.." has been executed!**",
+                                    ["description"] = "https://github.com",
+                                    ["type"] = "rich",
+                                    ["color"] = tonumber(0xffffff),
+                                    ["fields"] = {
+                                        {
+                                            ["name"] = "Display Name:",
+                                            ["value"] = game.Players.LocalPlayer.DisplayName,
+                                            ["inline"] = true
+                                        },
+                                        {
+                                            ["name"] = "User Name:",
+                                            ["value"] = game.Players.LocalPlayer.Name,
+                                            ["inline"] = true
+                                        },
+                                        {
+                                            ["name"] = "Hardware ID:",
+                                            ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
+                                            ["inline"] = true
+                                        }
                                     }
-                                }
-                            }}
-                        })
-                    }
-                )
-            end
-        else
-            if HardwareID then
-                local responce = syn.request(
-                    {
-                        Url = Webhook,
-                        Method = 'POST',
-                        Headers = {
-                            ['Content-Type'] = 'application/json'
-                        },
-                        Body = game:GetService("HttpService"):JSONEncode({
-                            ["content"] = "",
-                            ["embeds"] = {{
-                                ["title"] = "**"..Title.." has been executed!**",
-                                ["description"] = "https://github.com",
-                                ["type"] = "rich",
-                                ["color"] = tonumber(0xffffff),
-                                ["fields"] = {
-                                    {
-                                        ["name"] = "Display Name:",
-                                        ["value"] = game.Players.LocalPlayer.DisplayName,
-                                        ["inline"] = true
-                                    },
-                                    {
-                                        ["name"] = "User Name:",
-                                        ["value"] = game.Players.LocalPlayer.Name,
-                                        ["inline"] = true
-                                    },
-                                    {
-                                        ["name"] = "Hardware ID:",
-                                        ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
-                                        ["inline"] = true
+                                }}
+                            })
+                        }
+                    )
+                else
+                    local responce = syn.request(
+                        {
+                            Url = Webhook,
+                            Method = 'POST',
+                            Headers = {
+                                ['Content-Type'] = 'application/json'
+                            },
+                            Body = game:GetService("HttpService"):JSONEncode({
+                                ["content"] = "",
+                                ["embeds"] = {{
+                                    ["title"] = "**"..Title.." has been executed!**",
+                                    ["description"] = "https://github.com",
+                                    ["type"] = "rich",
+                                    ["color"] = tonumber(0xffffff),
+                                    ["fields"] = {
+                                        {
+                                            ["name"] = "Display Name:",
+                                            ["value"] = game.Players.LocalPlayer.DisplayName,
+                                            ["inline"] = true
+                                        },
+                                        {
+                                            ["name"] = "User Name:",
+                                            ["value"] = game.Players.LocalPlayer.Name,
+                                            ["inline"] = true
+                                        }
                                     }
-                                }
-                            }}
-                        })
-                    }
-                )
-            else
-                local responce = syn.request(
-                    {
-                        Url = Webhook,
-                        Method = 'POST',
-                        Headers = {
-                            ['Content-Type'] = 'application/json'
-                        },
-                        Body = game:GetService("HttpService"):JSONEncode({
-                            ["content"] = "",
-                            ["embeds"] = {{
-                                ["title"] = "**"..Title.." has been executed!**",
-                                ["description"] = "https://github.com",
-                                ["type"] = "rich",
-                                ["color"] = tonumber(0xffffff),
-                                ["fields"] = {
-                                    {
-                                        ["name"] = "Display Name:",
-                                        ["value"] = game.Players.LocalPlayer.DisplayName,
-                                        ["inline"] = true
-                                    },
-                                    {
-                                        ["name"] = "User Name:",
-                                        ["value"] = game.Players.LocalPlayer.Name,
-                                        ["inline"] = true
-                                    }
-                                }
-                            }}
-                        })
-                    }
-                )
+                                }}
+                            })
+                        }
+                    )
+                end
             end
         end
+
+        return dwm
     end
 
-    return dwm
+    return library
+else
+    game.StarterGui:SetCore("SendMessage", {
+        Title = "DWM",
+        Text = "Your Executor Doesn't Have The Function syn.request!",
+        Duration = 10,
+        Button1 = "OK"
+    })
 end
-
-return library
